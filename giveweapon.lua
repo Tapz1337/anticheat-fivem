1e7eab32e17a0466d5acf5fd712594f072c60ceb
TapzQ <109795631+TapzQ@users.noreply.github.com>
2023-02-28 15:08:41 +0100

    Create giveweapon.lua
    You need to set the Max Armor to 99. Or the player will get banned, if he tries to put on an armor.

---
+++ giveweapon.lua
@@ -0,0 +1,129 @@
+AC = {
+  Weapons = {
+    Ban = false,
+    Kick = false,
+    Blacklist = 
+            "WEAPON_HEAVYSHOTGUN",
+            "WEAPON_MICROSMG",
+            "WEAPON_MINISMG",
+            "WEAPON_MACHINEPISTOL",
+            "WEAPON_VINTAGEPISTOL",
+            "WEAPON_MARKSMANPISTOL",
+            "WEAPON_REVOLVER_MK2",
+            "WEAPON_DOUBLEACTION",
+            "WEAPON_RAYPISTOL",
+            "WEAPON_CERAMICPISTOL",
+            "WEAPON_NAVYREVOLVER",
+            "WEAPON_ASSAULTSHOTGUN",
+            "WEAPON_BULLPUPSHOTGUN",
+            "WEAPON_MUSKET",
+            "WEAPON_DBSHOTGUN",
+            "WEAPON_AUTOSHOTGUN",
+            "WEAPON_ASSAULTRIFLE_MK2",
+            "WEAPON_MG",
+            "WEAPON_COMBATMG",
+            "WEAPON_COMBATMG_MK2",
+            "WEAPON_HEAVYSNIPER",
+            "WEAPON_HEAVYSNIPER_MK2",
+            "WEAPON_MARKSMANRIFLE",
+            "WEAPON_MARKSMANRIFLE_MK2",
+            "WEAPON_RPG",
+            "WEAPON_GRENADELAUNCHER",
+            "WEAPON_GRENADELAUNCHER_SMOKE",
+            "WEAPON_MINIGUN",
+            "WEAPON_FIREWORK",
+            "WEAPON_RAILGUN",
+            "WEAPON_HOMINGLAUNCHER",
+            "WEAPON_COMPACTLAUNCHER",
+            "WEAPON_RAYMINIGUN",
+            "WEAPON_GRENADE",
+            "WEAPON_BZGAS",
+            "WEAPON_MOLOTOV",
+            "WEAPON_STICKYBOMB",
+            "WEAPON_PROXMINE",
+            "WEAPON_PIPEBOMB",
+            "WEAPON_SMOKEGRENADE",
+            "WEAPON_FIREEXTINGUISHER",
+            "WEAPON_HAZARDCAN"
+    }
+  },
+Explosions = {
+        {
+            label = "Explosion 29",
+            type = 29,
+            ban = true,
+            kick = false,
+            log = true
+        },
+        {
+            label = "Bird Crap",
+            type = 35,
+            ban = true,
+            kick = false,
+            log = true
+        },
+        {
+            label = "GrenadeLauncher",
+            type = 1,
+            ban = true,
+            kick = false,
+            log = true
+        },
+        {
+            label = "Rocket",
+            type = 4,
+            ban = true,
+            kick = false,
+            log = true
+        },
+        {
+            label = "Tank Shell",
+            type = 5,
+            ban = true,
+            kick = false,
+            log = true
+        },
+        {
+            label = "Bullet",
+            type = 18,
+            ban = true,
+            kick = false,
+            log = true
+        },
+        {
+            label = "SmokeGrenadeLauncher",
+            type = 19,
+            ban = true,
+            kick = false,
+            log = true
+        },
+        {
+            label = "Firework",
+            type = 35,
+            ban = false,
+            kick = false,
+            log = true
+        },
+        {
+            label = "Proxmine",
+            type = 37,
+            ban = true,
+            kick = false,
+            log = true
+        },
+        {
+            label = "Valkyrie_Cannon",
+            type = 38,
+            ban = true,
+            kick = false,
+            log = true
+        },
+        {
+            label = "Automizer",
+            type = 70,
+            ban = true,
+            kick = false,
+            log = true
+        }
+    }
+}
